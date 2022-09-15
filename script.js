622adaac3a29d9ad5212ae2fae305b34102bd120
Jeremy775 <jlaisne77@gmail.com>
2022-09-15 09:27:13 +0200

    suppression code inutile

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ dropdowns.forEach(dropdown => {
     const select = dropdown.querySelector('.select');
     const caret = dropdown.querySelector('.caret');
     const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelector('.options');
-    const selected = dropdown.querySelector('.selected');
 
     select.addEventListener('click', () => {
         // add the clicked select style to the select element
@@ -14,19 +12,5 @@ dropdowns.forEach(dropdown => {
         caret.classList.toggle('caret-rotate');
         // add the open style to the menu element
         menu.classList.toggle('menu-open');
-    });
-
-    options.forEach(option => {
-        // add click event to option elem
-        option.addEventListener('click', () => {
-            selected.innerText = option.innerText;
-            select.classList.remove('select-clicked');
-            caret.classList.remove('caret-rotate');
-            menu.classList.remove('menu-open');
-            options.forEach(option => {
-                option.classList.remove('active');
-            });
-            option.classList.add('active');
-        });
     });
 });
